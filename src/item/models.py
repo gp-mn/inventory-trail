393676d0623748421d776f4dd4a0fdaf279bf0b0
AlienIdeology <wcliaw610@gmail.com>
2023-07-09 10:39:36 -0500

    change default values

--- src/item/models.py
+++ src/item/models.py
@@ -3,11 +3,11 @@ from django.db import models
 class ItemGroup(models.Model):
     name = models.TextField(null=False, blank=False)
     location = models.TextField(null=False, blank=True)
-    num_items = models.IntegerField(default=0)
-    item_count = models.IntegerField(default=0)
+    num_items = models.IntegerField(default=1)
+    item_count = models.IntegerField(default=1)
     AVAIL = "AVAIL"
     UNAVAIL = "UNAVAIL"
     status = models.TextField(null=False, blank=False, 
                               choices=[(AVAIL, "available"), (UNAVAIL, "unavailable")], 
-                              default=UNAVAIL)
+                              default=AVAIL)
     description = models.TextField(null=False, blank=True)
