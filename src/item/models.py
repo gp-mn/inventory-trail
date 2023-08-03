70f5d3de95622cc5bc016ed5e5b61de938da6510
AlienIdeology <wcliaw610@gmail.com>
2023-08-02 19:18:34 -0500

    finish slug generated on itemgroup name, added dynamic routing for slugs

--- src/item/models.py
+++ src/item/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from hashid_field import HashidAutoField
 
 class ItemGroup(models.Model):
     name = models.TextField(null=False, blank=False)
-    tslug = HashidAutoField(primary_key=True)
-    # slug = models.SlugField(null=False, blank=True, unique=True)    # blank=True (or we can use default=?) so the item 
-    #                                                                 # can be created first before its slug is filled
+
+    # TODO obfuscation of slugs
+    slug = models.SlugField(null=False, blank=True, unique=True)    # blank=True (or we can use default=?) so the item 
+                                                                    # can be created first before its slug is filled
     location = models.TextField(null=False, blank=True)
     num_items = models.IntegerField(default=1)
     item_count = models.IntegerField(default=1)
@@ -15,19 +15,3 @@ class ItemGroup(models.Model):
                               choices=[(AVAIL, "available"), (UNAVAIL, "unavailable")], 
                               default=AVAIL)
     description = models.TextField(null=False, blank=True)
-
-#   dont need this if we're using hashid 
-#     def save(self, *args, **kwargs): # Override save function
-#         # If saving for the first time, call slugify() to set slug
-#         if not self.slug:
-#             self.slug = itemHash(self)
-#         super(ItemGroup, self).save(*args, **kwargs)
-
-# s = 0
-
-# def itemHash(itmgrp):
-#     # hashing function here
-#     # example:
-#     global s 
-#     s += 1
-#     return s
